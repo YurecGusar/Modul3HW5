9cbce6d658eeba01cb4af774e980706380443e6d
Yurii <leoonovn@gmail.com>
2021-08-11 15:25:03 +0300

    develop

--- Modul3HW5/Modul3HW5/Modul3HW5/Program.cs
+++ Modul3HW5/Modul3HW5/Modul3HW5/Program.cs
@@ -6,9 +6,7 @@ namespace Modul3HW5
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("develop");
         }
     }
 }
